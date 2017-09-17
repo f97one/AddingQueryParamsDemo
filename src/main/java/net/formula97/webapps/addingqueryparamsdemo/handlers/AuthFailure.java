153508f4d343835e53d4ff4867db8829e0bfb32e
HAJIME Fukuna <f97one@gmail.com>
2017-09-17 14:17:50 +0900

    エラー内容を分析して、RequestScopeセッションにそれを置く処理に変更。

--- src/main/java/net/formula97/webapps/addingqueryparamsdemo/handlers/AuthFailure.java
+++ src/main/java/net/formula97/webapps/addingqueryparamsdemo/handlers/AuthFailure.java
@@ -5,7 +5,13 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.security.authentication.AccountExpiredException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
@@ -19,14 +25,26 @@ public class AuthFailure implements AuthenticationFailureHandler {
         
         // add whole query parameters to url 
         String queryParams = request.getQueryString() == null ? "" : "?" + request.getQueryString();
+
+        // Analyze the cause of the error
+        String errReason = null;
+        if (exception instanceof BadCredentialsException) {
+            errReason = "Invalid user name or password.";
+        } else if (exception instanceof AccountExpiredException) {
+            errReason = "This account is expired. Please contact administrator.";
+        } else if (exception instanceof CredentialsExpiredException) {
+            errReason = "Your password is expired. Please contact administrator.";
+        } else if (exception instanceof DisabledException) {
+            errReason = "Your password is disabled. Please contact administrator.";
+        } else if (exception instanceof LockedException) {
+            errReason = "Your accouunt is locked. Please contact administrator.";
+        } else {
+            errReason = "Unknown problem occured. Please contact administrator.";
+        }
         
-        // add error parameter if not present
-        if (!queryParams.contains("error")) {
-            if (queryParams.length() == 0) {
-                queryParams = queryParams + "?error";
-            } else {
-                queryParams = queryParams + "&error";
-            }
+        if (errReason != null && errReason.length() > 0) {
+            HttpSession session = request.getSession();
+            session.setAttribute("errReason", errReason);
         }
 
         RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
